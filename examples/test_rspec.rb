74e45478ff5508a9834a3d9e5d9a1d71b4985301
Jochen <jochen@netlog.com>
2011-08-13 15:24:13 +0200

    rspec example

---
+++ examples/test_rspec.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+gem "rspec", ">=1.2.8"
+gem "selenium-client", ">=1.2.18"
+require "selenium/client"
+require "selenium/rspec/spec_helper"
+gem "testingbot"
+require "testingbot"
+
+describe "People" do
+  attr_reader :selenium_driver
+  alias :page :selenium_driver
+  
+  before(:all) do
+        @selenium_driver = Selenium::Client::Driver.new \
+            :host => "localhost", 
+            :port => 4444, 
+            :browser => "*safari", 
+            :url => "http://www.google.com", 
+            :timeout_in_second => 60
+    end
+
+    before(:each) do
+      selenium_driver.start_new_browser_session
+    end
+
+    append_after(:each) do 
+      @selenium_driver.close_current_browser_session
+    end
+
+    it "can find Selenium" do    
+      page.open "/"
+      page.title.should eql("Google")   
+    end
+end
