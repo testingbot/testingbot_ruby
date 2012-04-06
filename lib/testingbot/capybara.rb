3e4112b6ffe4dc697341f84e828e7e41f7e91658
Jochen <jochen@netlog.com>
2012-04-06 21:00:45 +0200

    capybara and selenium driver

---
+++ lib/testingbot/capybara.rb
@@ -0,0 +1,45 @@
+require 'capybara'
+require 'testingbot/config'
+require 'testingbot/tunnel'
+require 'capybara/dsl'
+
+@tunnel = nil
+
+module TestingBot
+	module Capybara
+
+		def start_tunnel
+			return @tunnel unless @tunnel.nil?
+
+			@tunnel = TestingBot::Tunnel.new
+			@tunnel.start
+		end
+
+		class CustomDriver < ::Capybara::Selenium::Driver
+			  def browser
+			    unless @browser
+			      if TestingBot.get_config[:need_tunnel]
+			      	TestingBot::Capybara.start_tunnel
+			      end
+
+			      @browser = TestingBot::SeleniumWebdriver.new
+
+			      main = Process.pid
+			      at_exit do
+			        @browser.quit if @browser
+			        if TestingBot.get_config[:need_tunnel]
+				      	@tunnel.stop unless @tunnel.nil?
+				    end
+			      end
+			    end
+			    @browser
+			  end
+		end
+	end
+end
+
+Capybara.register_driver :testingbot do |app|
+  TestingBot::Capybara::CustomDriver.new(app)
+end
+
+Capybara.default_driver = :testingbot
---
+++ lib/testingbot/selenium.rb
@@ -0,0 +1,107 @@
+# we create our own Selenium classes here to extend them with user credentials and config
+
+require 'selenium/client'
+require 'selenium/webdriver'
+require 'selenium/webdriver/remote/http/persistent'
+
+module TestingBot
+
+	class SeleniumWebdriver
+
+		attr_reader :config, :driver
+
+		def initialize(options = {})
+			@config = TestingBot::get_config
+			@config.add_options(options)
+
+			http_client = ::Selenium::WebDriver::Remote::Http::Persistent.new
+		    http_client.timeout = 400
+		    @driver = ::Selenium::WebDriver.for(:remote, :url => "http://#{@config[:client_key]}:#{@config[:client_secret]}@hub.testingbot.com:4444/wd/hub", :desired_capabilities => @config.desired_capabilities, :http_client => http_client)
+		    http_client.timeout = 120
+		end
+
+		def method_missing(meth, *args)
+	      @driver.send(meth, *args)
+	    end
+
+	    def stop
+	      @driver.quit
+	    end
+	end
+end
+
+# if selenium RC, add testingbot credentials to request
+if defined?(Selenium) && defined?(Selenium::Client) && defined?(Selenium::Client::Protocol)
+  module Selenium
+    module Client
+      module Protocol
+          # add custom parameters for testingbot.com
+          def http_request_for_testingbot(verb, args)
+            data = http_request_for_original(verb, args)
+            data << "&client_key=#{TestingBot.get_config[:client_key]}&client_secret=#{TestingBot.get_config[:client_secret]}"
+          end
+
+          begin
+           alias http_request_for_original http_request_for
+           alias http_request_for http_request_for_testingbot
+         rescue
+         end
+      end
+    end
+  end
+end
+
+if defined?(Selenium) && defined?(Selenium::Client) && defined?(Selenium::Client::Base)
+  module Selenium
+    module Client
+      module Base
+        DEFAULT_OPTIONS = {
+          :screenshot => true
+        }
+        
+        alias :close_current_browser_session_old :close_current_browser_session
+        alias :start_new_browser_session_old :start_new_browser_session
+        alias :initialize_old :initialize
+        
+        attr_accessor :options
+        attr_accessor :session_id_backup
+        attr_accessor :extra
+        attr_accessor :platform
+        attr_accessor :version
+        
+        def initialize(*args)
+          if args[0].kind_of?(Hash)
+            options = args[0]
+            @platform = options[:platform] || "WINDOWS"
+            @version  = options[:version]  if options[:version]
+          end
+          
+          @options = DEFAULT_OPTIONS
+          initialize_old(*args)
+          @host = "hub.testingbot.com" if @host.nil?
+          @port = 4444 if @port.nil?
+        end
+        
+        def close_current_browser_session
+          @session_id_backup = @session_id
+          close_current_browser_session_old
+        end
+        
+        def start_new_browser_session(options={})
+          options = @options.merge options
+          options[:platform] = @platform
+          options[:version] = @version unless @version.nil?
+          start_new_browser_session_old(options)
+        end
+        
+        def extra=(str)
+          @extra = str
+        end
+        
+        def options=(opts = {})
+          @options = @options.merge opts
+        end
+      end
+    end
+  end
+end
