f3cd2113fb62c51f81713c68808d58a2aff9885a
Jochen <jochen@netlog.com>
2012-04-06 20:59:12 +0200

    Android example + simplify capybara example

--- examples/capybara.rb
+++ examples/capybara.rb
@@ -1,23 +1,10 @@
 require 'rspec'
 require 'capybara/rspec'
-require 'selenium/webdriver'
 require 'testingbot'
+require 'testingbot/capybara'
 
-RSpec.configure do |config|
-	caps = Selenium::WebDriver::Remote::Capabilities.firefox  
-	caps.version = "8"  
-	caps.platform = :WINDOWS 
-
-	Capybara.default_driver = :testingbot
-	Capybara.register_driver :testingbot do |app|
-		client = Selenium::WebDriver::Remote::Http::Default.new
-  		client.timeout = 120
-	  	Capybara::Selenium::Driver.new(app,
-		    :browser => :remote,
-		    :url => "http://key:secret@hub.testingbot.com:4444/wd/hub",
-		    :http_client => client,
-		    :desired_capabilities => caps)
-	end
+TestingBot::config do |config|
+	config[:desired_capabilities] = { :browserName => "firefox", :version => 9, :platform => "WINDOWS" }
 end
 
 describe "People", :type => :request do
@@ -25,8 +12,8 @@ describe "People", :type => :request do
 	    Capybara.app_host = "http://testingbot.com"
  	end
 
-  it 'has a homepage with the word Selenium' do
-    visit '/'
-    page.should have_content('Selenium')
-  end
+	it 'has a homepage with the word Selenium' do
+		visit '/'
+		page.should have_content('Selenium')
+	end
 end
