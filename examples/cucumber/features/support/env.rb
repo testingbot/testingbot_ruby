8233d65de04c61d7df0e73d9b52c057e5f141355
Jochen <jochen@netlog.com>
2012-04-06 19:27:52 +0200

    update cucumber example

--- examples/cucumber/features/support/env.rb
+++ examples/cucumber/features/support/env.rb
@@ -4,17 +4,8 @@ require 'rspec'
 require 'selenium/webdriver'
 require 'testingbot/cucumber'
 
-caps = Selenium::WebDriver::Remote::Capabilities.firefox  
-caps.version = "8"  
-caps.platform = :WINDOWS 
+TestingBot::config do |config|
+	config[:desired_capabilities] = { :browserName => "firefox", :version => 9, :platform => "WINDOWS" }
+end
 
 Capybara.default_driver = :testingbot
-Capybara.register_driver :testingbot do |app|
-	client = Selenium::WebDriver::Remote::Http::Default.new
-		client.timeout = 120
-  	Capybara::Selenium::Driver.new(app,
-	    :browser => :remote,
-	    :url => "http://key:secret@hub.testingbot.com:4444/wd/hub",
-	    :http_client => client,
-	    :desired_capabilities => caps)
-end
--- examples/cucumber/features/youtube.feature
+++ examples/cucumber/features/youtube.feature
@@ -1,3 +1,4 @@
+@selenium
 Feature: YouTube has a search function.
 
 Background:
