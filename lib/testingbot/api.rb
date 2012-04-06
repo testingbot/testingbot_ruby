a619a2c7939a9c2ad667f733e62cddfbd3bdcb34
Jochen <jochen@netlog.com>
2012-04-06 17:47:02 +0200

    api Client + tests

---
+++ lib/testingbot/api.rb
@@ -0,0 +1,112 @@
+require 'json'
+module TestingBot
+
+	class Api
+
+		VERSION = 1
+		API_URL = "http://api.testingbot.com"
+
+		attr_reader :config
+
+		def initialize(opts = {})
+			@config = TestingBot::Config.new(opts)
+		end
+
+		def get_user_info
+			get("/user")
+		end
+
+		def update_user_info(params = {})
+			new_params = {}
+			params.keys.each do |key|
+				new_params["user[#{key}]"] = params[key]
+			end
+			response = put("/user", new_params)
+			response["success"]
+		end
+
+		def get_tests(offset = 0, count = 10)
+			get("/tests")
+		end
+
+		def get_single_test(test_id)
+			get("/tests/#{test_id}")
+		end
+
+		def update_test(test_id, params = {})
+			new_params = {}
+			params.keys.each do |key|
+				new_params["test[#{key}]"] = params[key]
+			end
+			response = put("/tests/#{test_id}", new_params)
+			response["success"]
+		end
+
+		def delete_test(test_id)
+			response = delete("/tests/#{test_id}")
+			response["success"]
+		end
+
+		private
+
+		def get(url)
+			uri = URI(API_URL + '/v' + VERSION.to_s + url)
+        	req = Net::HTTP::Get.new(uri.request_uri)
+        	req.basic_auth @config[:client_key], @config[:client_secret]
+        	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+			  http.request(req)
+			}
+
+			parsed = JSON.parse(res.body)
+
+			if !parsed["error"].nil? && !parsed["error"].empty?
+				raise parsed["error"]
+			end
+
+			parsed
+		end
+
+		def put(url, params = {})
+			uri = URI(API_URL + '/v' + VERSION.to_s + url)
+        	req = Net::HTTP::Put.new(uri.request_uri)
+        	req.basic_auth @config[:client_key], @config[:client_secret]
+        	req.set_form_data(params)
+        	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+			  http.request(req)
+			}
+
+			parsed = JSON.parse(res.body)
+
+			if !parsed["error"].nil? && !parsed["error"].empty?
+				raise parsed["error"]
+			end
+
+			parsed
+		end
+
+		def delete(url, params = {})
+			uri = URI(API_URL + '/v' + VERSION.to_s + url)
+        	req = Net::HTTP::Delete.new(uri.request_uri)
+        	req.basic_auth @config[:client_key], @config[:client_secret]
+        	req.set_form_data(params)
+        	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+			  http.request(req)
+			}
+
+			parsed = JSON.parse(res.body)
+
+			if !parsed["error"].nil? && !parsed["error"].empty?
+				raise parsed["error"]
+			end
+
+			parsed
+		end
+
+		def post(url, params = {})
+        	url = URI.parse(API_URL + '/v' + VERSION + url)
+        	http = Net::HTTP.new(url.host, url.port)
+        	http.basic_auth @config[:client_key], @config[:client_secret]
+        	response = http.post(url.path, params.map { |k, v| "#{k.to_s}=#{v}" }.join("&"))
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/config.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/tunnel.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/api.rb')
