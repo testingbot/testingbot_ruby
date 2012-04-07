4fbe1513d900b05f34bb71455737618f2901aa22
Jochen <jochen@netlog.com>
2012-04-07 12:03:13 +0200

    add testingbot main file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/config.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/tunnel.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot/api.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/testingbot.rb')
