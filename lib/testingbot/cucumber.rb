32eb96c34447146e5a2fc22c0688afafdedfd1fa
Jochen <jochen@netlog.com>
2012-04-09 19:34:36 +0200

    use API for reporting + fix fetching session_id for cucumber

--- lib/testingbot/cucumber.rb
+++ lib/testingbot/cucumber.rb
@@ -1,5 +1,6 @@
 require 'testingbot/config'
 require 'testingbot/capybara'
+require 'testingbot/api'
 
 if defined?(Cucumber)
     Before('@selenium') do
@@ -10,21 +11,22 @@ if defined?(Cucumber)
         if !TestingBot.get_config[:client_key].nil?
             begin
                 driver = ::Capybara.current_session.driver
-                session_id = driver.browser.instance_variable_get("@bridge").instance_variable_get("@session_id")
-                  
+                if driver.browser.respond_to?(:session_id)
+                    session_id = driver.browser.session_id
+                else
+                    session_id = driver.browser.instance_variable_get("@bridge").instance_variable_get("@session_id")
+                end
+
+                api = TestingBot::Api.new
                 params = {
                     "session_id" => session_id,
-                    "client_key" => TestingBot.get_config[:client_key],
-                    "client_secret" => TestingBot.get_config[:client_secret],
                     "status_message" => (scenario.failed? ? scenario.exception.message : ""),
-                    "success" => !scenario.failed?,
+                    "success" => !scenario.failed? ? 1 : 0,
                     "name" => scenario.title,
                     "kind" => 2
                 }
-                
-                url = URI.parse('http://testingbot.com/hq')
-                http = Net::HTTP.new(url.host, url.port)
-                response = http.post(url.path, params.map { |k, v| "#{k.to_s}=#{v}" }.join("&"))
+
+                data = api.update_test(session_id, params)
             rescue Exception => e
                 p "Could not determine sessionID, can not send results to TestingBot.com #{e.message}"
             end
